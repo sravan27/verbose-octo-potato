480350a283c2f9d1daadecfb4b37d943fe9a23f6
sravan27 <sridharsravan@live.com>
2024-12-19 16:15:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,13 @@ setup(
     name="zlm",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "jinja2",
+        "streamlit",
+        "nltk",
+        "openai"
+    ],
+    author="Your Name",
+    description="Your custom description",
+    url="https://github.com/sravan27/verbose-octo-potato",
 )
